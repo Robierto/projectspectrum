b1b68a9d1cf447d4375402027c9b32bba0118a76
Tom <hainest1@nku.edu>
2016-07-21 21:57:58 -0400

    paintables

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -14,24 +14,26 @@ public class PlayerShoot : MonoBehaviour {
 	public Material tankMaterial;
 
 	// Use this for initialization
-	void Start () {
+	void Start() {
 		//Color redTank = Color.red;
 		//tankMaterial.SetColor ("_EmissionColor", redTank);
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update() {
 		if (Input.GetButtonDown ("Fire1")) 
 		{
 			GameObject _clone = Instantiate (bulletPrefabs [0], primaryFirePoint.position, primaryFirePoint.rotation) as GameObject;
-			_clone.GetComponent<Rigidbody> ().AddForce (primaryFirePoint.transform.forward * primaryFireForce, ForceMode.Impulse);
+			_clone.GetComponent<SmearEffect>().enabled = true;
+			_clone.GetComponent<Rigidbody>().AddForce(primaryFirePoint.transform.forward * primaryFireForce, ForceMode.Impulse);
 			Destroy (_clone, 10.0f);
 		}
 
 		if (Input.GetButtonDown ("Fire2")) 
 		{
 			GameObject _clone = Instantiate (bulletPrefabs [1], secondaryFirePoint.position, secondaryFirePoint.rotation) as GameObject;
-			_clone.GetComponent<Rigidbody> ().AddForce ( secondaryFirePoint.transform.forward * secondaryFireForce, ForceMode.Impulse);
+			_clone.GetComponent<SmearEffect>().enabled = true;
+			_clone.GetComponent<Rigidbody>().AddForce(secondaryFirePoint.transform.forward * secondaryFireForce, ForceMode.Impulse);
 			Destroy(_clone, 10.0f);
 		}
 	}
---
+++ Assets/Scripts/Splatter.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Splatter : MonoBehaviour {
+
+	public Color colorToUse;
+
+	public float despawnTime = 10.0f;
+
+	public GameObject splatterPrefab;
+
+	public float splatterForce = 20.0f;
+
+	public float splatterRadius = 5.0f;
+
+	private Rigidbody rb;
+	private MeshRenderer mesh;
+	private Collider col;
+
+	void Start () {
+		rb = gameObject.GetComponent<Rigidbody>();
+		mesh = gameObject.GetComponent<MeshRenderer>();
+		col = gameObject.GetComponent<Collider>();
+	}
+
+	void Update () {
+		despawnTime -= Time.deltaTime;
+		if (despawnTime <= 0)
+			Destroy(gameObject);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Paintable")
+		{
+			other.GetComponent<MeshRenderer>().material.color = colorToUse;
+			DoSplatter();
+			Debug.Log("Hit Paintable");
+		}
+	}
+
+	void DoSplatter()
+	{
+		rb.constraints = RigidbodyConstraints.FreezeAll;
+		mesh.enabled = false;
+		col.enabled = false;
+
+		GameObject _clone = Instantiate(splatterPrefab, transform.position, transform.rotation) as GameObject;
+		MeshRenderer[] renderers = _clone.GetComponentsInChildren<MeshRenderer>();
+		foreach (MeshRenderer mr in renderers)
+		{
+			mr.material.color = colorToUse;
+		}
+		_clone.GetComponent<Rigidbody>().AddExplosionForce(splatterForce, _clone.transform.position, splatterRadius);
+		Destroy(_clone, 8.0f);
+	}
+}
